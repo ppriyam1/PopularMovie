452b98922adafd54838a2ea12bc5c760d3ce9966
Preeti Priyam <ppriyam1@binghamton.edu>
2021-07-02 05:21:06 -0400

    Initial commit

---
+++ PopularMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopularMovie
+//
+//  Created by Preeti Priyam on 7/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
